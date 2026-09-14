c18c4658eed7d8db1efa313217c9c3ee0b09af35
EVGBuild <kdygsd44hg@privaterelay.appleid.com>
2026-09-14 11:03:46 +0200

    i18n: load locale files before app bootstrap

--- js/core/i18n.js
+++ js/core/i18n.js
@@ -1,6 +1,11 @@
+window.LUMI_LOCALES=window.LUMI_LOCALES||{};
+if(!window.LUMI_LOCALES.nl||!window.LUMI_LOCALES.en){
+  document.write('<script src="./js/i18n/locales/nl.js?v=1"><\/script><script src="./js/i18n/locales/en.js?v=1"><\/script>');
+}
+
 const LUMI_SUPPORTED_LOCALES=['nl','en'];
 const LUMI_FALLBACK_LOCALE='nl';
-const LUMI_TRANSLATIONS=window.LUMI_LOCALES||{};
+const LUMI_TRANSLATIONS=window.LUMI_LOCALES;
 
 function normalizeLumiLocale(code){
   const value=String(code||'').trim().toLowerCase();
